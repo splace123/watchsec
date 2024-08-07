99aa28a492cb706d3466f7f67c17583d6e0f2e24
splace123 <146458799+splace123@users.noreply.github.com>
2024-08-07 17:55:40 +0100

    saba

---
+++ index.js
@@ -0,0 +1,16 @@
+'use client';
+import HomePage from '@/components/pages/home/HomePage'
+
+
+export default function Home() {
+  return (
+   <>
+     <HomePage />
+     
+
+     
+
+   </>
+
+  );
+}
